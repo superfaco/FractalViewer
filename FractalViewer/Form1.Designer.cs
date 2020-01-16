c0667022f6990b3765ef5cda3a6ceed2f49bf784
Fernando Alfonso Caldera Olivas <superfaco1@gmail.com>
2020-01-16 01:57:30 -0600

    Added basic controls to the form. Added random fractal tree generation. Added save button functionality.

--- FractalViewer/Form1.Designer.cs
+++ FractalViewer/Form1.Designer.cs
@@ -28,13 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cbxFractals = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.savePictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.cbxFractals);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // cbxFractals
+            // 
+            this.cbxFractals.FormattingEnabled = true;
+            this.cbxFractals.Items.AddRange(new object[] {
+            "Random tree",
+            "Sierpinski triangule"});
+            this.cbxFractals.Location = new System.Drawing.Point(15, 38);
+            this.cbxFractals.Name = "cbxFractals";
+            this.cbxFractals.Size = new System.Drawing.Size(233, 21);
+            this.cbxFractals.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select a fractal to display:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Control Panel";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.savePictureToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // savePictureToolStripMenuItem
+            // 
+            this.savePictureToolStripMenuItem.Name = "savePictureToolStripMenuItem";
+            this.savePictureToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.savePictureToolStripMenuItem.Text = "Save picture";
+            this.savePictureToolStripMenuItem.Click += new System.EventHandler(this.savePictureToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "*.bmp";
+            this.saveFileDialog1.FileName = "image.bmp";
+            this.saveFileDialog1.Filter = "BMP|*.bmp";
+            this.saveFileDialog1.SupportMultiDottedExtensions = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Fractal Viewer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox cbxFractals;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem savePictureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- FractalViewer/Form1.cs
+++ FractalViewer/Form1.cs
@@ -12,9 +12,129 @@ namespace FractalViewer
 {
     public partial class Form1 : Form
     {
+        
+        private enum Fractal
+        {
+            RandomTree = 0,
+            SierpinskiTriangule
+        };
+
+        private int maxTreeIterations;
+        private Bitmap bmp;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        #region Fractal draw methods
+        #region Random tree methods
+        private void DrawRandomTree(Bitmap bmp)
+        {
+            //Margin percentages.
+            int topMarginPercentage = 10;
+            int bottomMarginPercentage = 10;
+
+            //For some randomness.
+            Random random = new Random();
+            maxTreeIterations = random.Next(9, 16);
+
+            //We draw the tree trunk first.
+            //It'll be aligned horizontally and its length is gonna be
+            //half the height minus the margin.
+            //We'll use polar coordinates btw.
+            double treeTrunkOriginX = bmp.Width / 2.0;
+            double treeTrunkOriginY = bmp.Height - (bottomMarginPercentage / 100.0 * bmp.Height);
+            double treeTrunkLength = (bmp.Height - (bottomMarginPercentage / 100.0 * bmp.Height) 
+                                        - (topMarginPercentage / 100.0 * bmp.Height)) / 3.0;
+            //We want the trunk vertical, so the angle must be -90°, since our origin is beneath the final point
+            //otherwise, i'll rotate down.
+            double treeTrunkAngle = -Math.PI / 2.0;
+            //We calculate the final X and Y coordinates using trigonometric functions.
+            double treeTrunkFinalX = (treeTrunkOriginX + treeTrunkLength * Math.Cos(treeTrunkAngle));
+            double treeTrunkFinalY = (treeTrunkOriginY + treeTrunkLength * Math.Sin(treeTrunkAngle));
+            
+            //Create a graphics object from the bmp, so we can access all the methods.
+            Graphics g = Graphics.FromImage(bmp);
+            //Dibujamos el fondo.
+            g.DrawRectangle(Pens.White, 0, 0, bmp.Width, bmp.Height);
+            //We simply draw the line.
+            //First, let's create a starting random pen color.
+            Color penColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256), random.Next(256));
+            //And a final pen color as well.
+            Color finalPenColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256), random.Next(256));
+            float penWidth = 15;
+            g.DrawLine(new Pen(penColor, penWidth), (float)treeTrunkOriginX, (float)treeTrunkOriginY, (float)treeTrunkFinalX, (float)treeTrunkFinalY);
+
+            //Now, we start drawing the branches.
+            //We'll reduce the width of the pen, as well as alter the color of the branches, based on the iteration.
+            //The length, at first, will not be changed.
+            //First the left one, so that's -45°.
+            _DrawBranchesRecursively(g, treeTrunkFinalX, treeTrunkFinalY, treeTrunkAngle - Math.PI / 4.0, treeTrunkLength * 0.7, (penColor.ToArgb() + (finalPenColor.ToArgb() - penColor.ToArgb()) / maxTreeIterations), (finalPenColor.ToArgb() - penColor.ToArgb()) / maxTreeIterations, penWidth - (penWidth - 1.0f) / maxTreeIterations, (penWidth - 1.0f) / maxTreeIterations, 1);
+            //Then the right one, +45°.
+            _DrawBranchesRecursively(g, treeTrunkFinalX, treeTrunkFinalY, treeTrunkAngle + Math.PI / 4.0, treeTrunkLength * 0.7, (penColor.ToArgb() + (finalPenColor.ToArgb() - penColor.ToArgb()) / maxTreeIterations), (finalPenColor.ToArgb() - penColor.ToArgb()) / maxTreeIterations, penWidth - (penWidth - 1.0f) / maxTreeIterations, (penWidth - 1.0f) / maxTreeIterations, 1);
+        }
+
+        private void _DrawBranchesRecursively(Graphics g, double originX, double originY, double angle, double length, int penColor, int penColorIncrement, float penWidth, float penWidthIncrement, int iteration)
+        {
+            if(iteration <= maxTreeIterations)
+            {
+                //We calculate the final X and Y coordinates.
+                double finalX = originX + length * Math.Cos(angle);
+                double finalY = originY + length * Math.Sin(angle);
+
+                //Now, let's draw the line.
+                g.DrawLine(new Pen(Color.FromArgb(penColor), penWidth), (float)originX, (float)originY, (float)finalX, (float)finalY);
+
+                //Now, we calculate how much the length is to be reduced..
+
+                double newLength = length * 0.7;
+
+                _DrawBranchesRecursively(g, finalX, finalY, angle - Math.PI / 4.0, newLength, (penColor + penColorIncrement), penColorIncrement, penWidth - penWidthIncrement, penWidthIncrement, iteration + 1);
+                _DrawBranchesRecursively(g, finalX, finalY, angle + Math.PI / 4.0, newLength, (penColor + penColorIncrement), penColorIncrement, penWidth - penWidthIncrement, penWidthIncrement, iteration + 1);
+            }
+        }
+        #endregion
+        #endregion
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if(bmp != null)
+            {
+                e.Graphics.DrawImage(bmp, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fractal fractal = (Fractal)cbxFractals.SelectedIndex;
+
+            if (Enum.IsDefined(typeof(Fractal), fractal))
+            {
+                bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                switch (fractal)
+                {
+                    case Fractal.RandomTree:
+                        DrawRandomTree(bmp);
+                        break;
+                    case Fractal.SierpinskiTriangule:
+                        break;
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void savePictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bmp.Save(saveFileDialog1.FileName);
+            }
         }
     }
 }
